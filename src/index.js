7e55adcbc99b84fdce1e30bd575964323098e157
redhawkIT <khodkodmitry10011983@hotmail.com>
2016-11-03 07:36:30 -0500

    update

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {createStore, applyMiddleware, combineReducers} from 'redux'
+import {Provider} from 'react-redux'
+import thunk from 'redux-thunk'
+import App from './App'
+import './index.css'
+
+import * as reducers from './store/reducers'
+const store = createStore(combineReducers(reducers), applyMiddleware(thunk))
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+  <Provider store={store}>
+  <App/>
+</Provider>, document.getElementById('root'))
